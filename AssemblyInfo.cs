90d7fd7801d3033e6f390d9dc2d2ce25ecfe2b74
allxone <allxone@hotmail.com>
2018-04-07 11:53:16 +0200

    tested

---
+++ AssemblyInfo.cs
@@ -0,0 +1,2 @@
+using log4net;
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using log4net;
+
+namespace test_log4net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            log.Info("Hello World! from log4net");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
